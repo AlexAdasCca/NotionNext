4bed95494f24e62eebfeb835c249e77d25ba8f4a
CthRio <3500197013@qq.com>
2025-07-04 19:18:21 +0800

    尝试微调顶栏颜色问题

--- themes/hexo/components/Header.js
+++ themes/hexo/components/Header.js
@@ -152,7 +152,7 @@ const Header = props => {
         id='sticky-nav'
         style={{ backdropFilter: 'blur(3px)' }}
         className={
-          'top-0 duration-300 transition-all  shadow-none from-black/45 fixed bg-none dark:bg-hexo-black-gray dark:text-gray-200 text-black w-full z-20 transform border-transparent dark:border-transparent'
+          'top-0 duration-300 transition-all  shadow-none bg-gradient-to-b from-black/45 fixed bg-none dark:bg-hexo-black-gray dark:text-gray-200 text-black w-full z-20 transform border-transparent dark:border-transparent'
         }>
         <div className='w-full flex justify-between items-center px-4 py-2'>
           <div className='flex'>
